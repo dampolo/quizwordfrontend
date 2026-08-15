e33edbc123f08f315e6da7e34ef492766120fa4b
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-15 09:49:24 +0200

    action adjusted

--- src/quiz/my-quiz/all-words.jsx
+++ src/quiz/my-quiz/all-words.jsx
@@ -171,12 +171,13 @@ function AllWords() {
                 <span>Days</span>
               </div>
 
-              <Link
-                to={`/my-quiz/${word.id}/edit-word?target-word=${word.translations[1].id}&language=${word.translations[1].language}`}
-                className="actions"
-              >
-                ✏️
-              </Link>
+              <div className="actions">
+                <Link
+                  to={`/my-quiz/${word.id}/edit-word?target-word=${word.translations[1].id}&language=${word.translations[1].language}`}
+                >
+                  ✏️
+                </Link>
+              </div>
             </div>
           ))
         )}

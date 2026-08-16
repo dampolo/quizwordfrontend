3b4d9907ab4224d00daaa70321ce5dab501f8b2e
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-16 08:30:18 +0200

    info with no quiz works

--- src/quiz/my-quiz/quizzes/AllQuizzes/all-quizzes.jsx
+++ src/quiz/my-quiz/quizzes/AllQuizzes/all-quizzes.jsx
@@ -89,32 +89,40 @@ function Quizzes() {
 
       {/* Quiz */}
       <div className="category">
-        {quizzes.map((quiz) => (
-          <article className="vocab-card" key={quiz.quiz_id}>
-            <h3>{quiz.quiz_name}</h3>
-
-            <div className="vocab-card__footer">
-              <Link
-                to={`/my-quiz/${quiz.quiz_id}/all-quiz-words`}
-                className="vocab-card__meta"
-              >
-                <span>▦</span>
-                <strong>{quiz.concepts_count} Words</strong>
-              </Link>
-
-              <div className="vocab-card__updated">
-                <span>Aktualisiert</span>
-                <strong>
-                  {new Date(quiz.updated_at).toLocaleDateString("de-DE", {
-                    day: "2-digit",
-                    month: "2-digit",
-                    year: "numeric",
-                  })}
-                </strong>
+        {quizzes.length === 0 ? (
+          <p className="no-quiz">Du hast hier kein Quiz erstellt.</p>
+        ) : (
+          
+          quizzes.map((quiz) => (
+            <article className="vocab-card" key={quiz.quiz_id}>
+              <h3>{quiz.quiz_name}</h3>
+  
+              <div className="vocab-card__footer">
+                <Link
+                  to={`/my-quiz/${quiz.quiz_id}/all-quiz-words`}
+                  className="vocab-card__meta"
+                >
+                  <span>▦</span>
+                  <strong>{quiz.concepts_count} Words</strong>
+                </Link>
+  
+                <div className="vocab-card__updated">
+                  <span>Aktualisiert</span>
+                  <strong>
+                    {new Date(quiz.updated_at).toLocaleDateString("de-DE", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    })}
+                  </strong>
+                </div>
               </div>
-            </div>
-          </article>
-        ))}
+            </article>
+          ))
+        )
+
+        }
+
         {/* Quiz END */}
 
         {/* <Link className="add-card" to="/my-quiz/add-new-quiz">

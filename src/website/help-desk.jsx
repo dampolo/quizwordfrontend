048d1fdec957051fd76a5f480557b0b83b1b0ec9
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-18 17:17:32 +0200

    help desk/FAQ update

--- src/website/help-desk.jsx
+++ src/website/help-desk.jsx
@@ -4,17 +4,28 @@ import "./help-desk.scss";
 function HelpDesk() {
   return (
     <main className="help-desk-main">
-
       <section className="help-desk">
-      <BackButton to="/"  className="back-button" />
+        <BackButton to="/" className="back-button" />
         <h1>Die häufigsten Fragen</h1>
+        
         <div className="help-desk-list-content">
           <details>
             <summary class="help-desk-button collapsible">
               Was kostet der Quiz Word?
             </summary>
             <div class="help-desk-list-content">
-                Der Quiz word is komplett kostenlos.
+              Der Quiz word is komplett kostenlos.
+            </div>
+          </details>
+        </div>
+
+        <div className="help-desk-list-content">
+          <details>
+            <summary class="help-desk-button collapsible">
+              Für wen ist der Quiz Word?
+            </summary>
+            <div class="help-desk-list-content">
+              Der Quiz Word wurde erstellt für alle Menschen die gerne Sprachen lernen.
             </div>
           </details>
         </div>

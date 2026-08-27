31ee2a4c2aafb6bcfd481004a54f966f912a43c4
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-27 13:36:23 +0200

    contact first component

---
+++ src/website/Contact/contact.jsx
@@ -0,0 +1,16 @@
+import PageTitle from "../../components/PageTitle/PageTitle";
+import BackButton from "../components/BackButton/BackButton";
+import "./contact.scss";
+
+function Contact() {
+
+    return(
+        <main className="contact-main">
+            <PageTitle title="Kontakt" />
+            <h1 className="title">Kontakt</h1>
+        </main>
+    )
+    
+}
+
+export default Contact

429fc9b922cd597b481f4bf9aa95f72ff9e8fc8f
Damian Poloczek <damianpoloczek@gmail.com>
2026-09-23 21:49:45 +0200

    change of Header

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/header";
+import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
 import "./PublicLayout.scss";
 
